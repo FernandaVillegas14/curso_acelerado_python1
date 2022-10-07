59504a079f68c6d679bbd3778ed76a0a449b6c89
Replit user <>
2022-10-07 18:52:09 +0000

    Ejercicio de la primera sesion

---
+++ sesion1/ejercicio1.py
@@ -0,0 +1,16 @@
+'''
+****Curso de programacion acelerada de Python****
+Date 007/10/2022
+File: sesion1/ejercicio1.py
+Autor: Maria Fernanda Martinez Villegas
+Accion: Asignacion de variables
+'''
+
+nombre_estado = 'Tabasco'
+superficie=25267
+poblacion_total =2403000000
+promedio_temperatura = 31.3
+productos_tabasco = ['Cacao', 'coco', 'Caña']
+print (nombre_estado, "es un estado que produce", )
+print(productos_tabasco[0], "y")
+print("Tiene una superficie de", superficie)
